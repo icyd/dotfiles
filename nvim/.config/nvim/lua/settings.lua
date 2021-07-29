be1dfec77a15fc175bd6ce6b62404553063d7207
Alberto Vázquez <beto.v25@gmail.com>
2021-07-28 23:27:37 -0500

    Updated tmux conf and disable python3 provider for nvim

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -63,8 +63,7 @@ g.loaded_python_provider = 0
 g.loaded_node_provider = 0
 g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
-local pyenv = os.getenv("PY_VENV")
-g.python3_host_prog = pyenv..'/pynvim/bin/python'
+g.loaded_python3_provider = 0
 
 g.netrw_banner = 0
 g.netrw_liststyle = 3
