5bbc027349aac1cba16c522cf64d280ad99f34e6
Alberto Vázquez <avazquez@contractor.ea.com>
2022-01-11 09:59:32 +0100

    fix: removed commented section

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,11 +3,6 @@ vim.g.did_load_filetypes = 1
 pcall(require, 'impatient')
 require('settings')
 require('keybindings')
--- pcall(require, 'packer_compiled')
-
--- vim.defer_fn(function()
---    require('plugins/packer')
--- end, 0)
 
 if root then
    set.shada = ''
