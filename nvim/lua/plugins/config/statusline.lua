1538dc3c60e49302ce774a0dd9d4a41e05562693
Alberto Vázquez <beto.v25@gmail.com>
2022-08-31 23:14:53 +0200

    test neogit

--- nvim/lua/plugins/config/statusline.lua
+++ nvim/lua/plugins/config/statusline.lua
@@ -6,47 +6,69 @@ function M.setup()
     end
 
     require('lualine').setup {
-      options = {
-        icons_enabled = true,
-        theme = 'kanagawa',
-        component_separators = {'', ''},
-        section_separators = {'', ''},
-        disabled_filetypes = {}
-      },
-      sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff'},
-        lualine_c = {
-            {
-                'filename',
-                file_status = true,
-                path = 3,
-                shorting_target = 40,
-            },
+        options = {
+            icons_enabled = true,
+            theme = 'kanagawa',
+            component_separators = {'', ''},
+            section_separators = {'', ''},
+            disabled_filetypes = {
+                "help",
+                "startify",
+                "dashboard",
+                "packer",
+                "neogitstatus",
+                "NvimTree",
+                "Trouble",
+                "toggleterm",
+            }
         },
-        lualine_x = {
-            {
-                'diagnostics',
-                sources={'nvim_diagnostic'}
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff'},
+            lualine_c = {
+                {
+                    'filename',
+                    file_status = true,
+                    path = 3,
+                    shorting_target = 40,
+                },
             },
-            'encoding',
-            'fileformat',
-            'filetype',
-            maximize_status
+            lualine_x = {
+                {
+                    'diagnostics',
+                    sources={'nvim_diagnostic'}
+                },
+                'encoding',
+                'fileformat',
+                'filetype',
+                maximize_status
+            },
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
         },
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-      },
-      tabline = {},
-      extensions = {}
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        },
+        tabline = {},
+        extensions = { "nvim-tree", "toggleterm", "quickfix" },
+        winbar = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {
+                {
+                    'filename',
+                    file_status = true,
+                },
+            },
+        }
     }
 end
 
